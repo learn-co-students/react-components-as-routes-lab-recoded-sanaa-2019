aad72554f403ef0f73444e953457ab44a2fab6dc
MohammedAlMaqbli <Mohammed.AlMaqbli@gmail.com>
2019-11-20 03:52:16 -0800

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+      <NavLink to="/" style={link} exact>
+        Home
+      </NavLink>
+      <NavLink to="/movies" style={link}>
+        Movies
+      </NavLink>
+      <NavLink to="/actors" style={link}>
+        Actors
+      </NavLink>
+      <NavLink to="/directors" style={link}>
+        Directors
+      </NavLink>
     </div>
   );
 };
 
-export default NavBar;
+const link = {
+  width: "100px",
+  padding: "12px",
+  margin: "0 10px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white"
+};
+export default NavBar
