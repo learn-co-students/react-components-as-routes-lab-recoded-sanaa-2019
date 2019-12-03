11d80d41f537c0d8d216e893b8c05722e87460c2
Eman Al-zariey <eman_alzariey@yahoo.com>
2019-12-03 10:52:58 +0000

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,8 +4,20 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
-    </div>
+        <h1>Movies Page</h1>
+
+          {movies.map( movie =>
+
+            <div className="movies">
+             {movie.title}
+             {movie.time}
+             <ul>
+            {movie.genres.map( movie => <li>{movie}</li>)}
+            </ul>
+            </div>
+
+            )}
+        </div>
   );
 };
 
