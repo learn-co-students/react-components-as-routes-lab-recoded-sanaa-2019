c7bdd1cc7f8e6df5b1bef255f27cc7dfff679c80
TaghreedNageeb <taghreed.n.d2018@gmail.com>
2019-12-09 13:19:57 -0800

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -5,6 +5,15 @@ const Directors = () => {
   return (
     <div>
       {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map(director=>
+      <div>
+        <h3>{director.name}</h3>
+        <ul>
+        {director.movies.map(movie=><li>{movie}</li>)}
+        </ul>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,8 +4,18 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
-    </div>
+    {/*{code here}*/}
+    <h1>Movies Page</h1>
+    {movies.map(movie=>
+    <div>
+      <h3>{movie.title}</h3>
+      <span>{movie.time}</span>
+      <ul>
+      {movie.genres.map(genre=><li>{genre}</li>)}
+      </ul>
+      </div>
+    )}
+  </div>
   );
 };
 
