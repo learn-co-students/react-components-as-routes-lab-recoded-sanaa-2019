bbf5f18a3d0ecf516d8a4c8dffa1648119c92c33
AISHA-PRO <aisha1rashed@gmail.com>
2019-12-17 00:22:57 -0800

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,9 +4,27 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <NavLink to="/" exact>
+        Home
+      </NavLink>
+      <NavLink to="/movies" >
+        Movies
+      </NavLink>
+      <NavLink to="/actors" >
+        Actors
+      </NavLink>
+      <NavLink to="/directors" >
+        Directors
+      </NavLink>
     </div>
   );
 };
-
+const link = {
+  width: "100px",
+  padding: "12px",
+  margin: "0 10px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white"
+};
 export default NavBar;
