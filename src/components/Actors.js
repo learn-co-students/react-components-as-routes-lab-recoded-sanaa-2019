d6bd7535917bbafc3c232af98a53ab7572f84237
learn <mohammedazizbuss@gmail.com>
2019-12-17 00:10:23 -0800

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,11 +2,22 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  let mapper=()=>{
+    return actors.map(e=>{
+     return <div>{[<h4>Name: {e.name}</h4>,
+      <p>Movies</p>,
+    <ul>{e.movies.map(a=>{
+    return <li>{a}</li>
+    })}</ul>]}</div>
+    })
+  }
   return (
     <div>
       {/*{code here}*/}
-    </div>
-  );
+      <h1>Actors Page</h1>
+      {mapper()}
+      </div>
+  )
 };
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,23 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  let mapper=()=>{
+    return movies.map(e=>{
+     return <div>
+       { [<h4>Title: {e.title}</h4>,
+     <p>Time {e.time}</p>,
+      <p>Geners</p>,
+    <ul>{e.genres.map(a=>{
+    return <li>{a}</li>
+    })}</ul>]}
+     </div>
+    })
+  }
   return (
     <div>
         {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {mapper()}
     </div>
   );
 };
